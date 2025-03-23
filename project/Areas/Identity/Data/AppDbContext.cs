a3c1eb42224c284bd8cf0e6f23da11c25cb74489
Bhushan Alhat <alhatbhushan@gmail.com>
2025-03-23 22:27:04 +0530

    Add project files.

---
+++ project/Areas/Identity/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using project.Areas.Identity.Data;
+
+namespace project.Areas.Identity.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new AppUserEntityConfiguration());
+    }
+}
+
+public class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.Name).HasMaxLength(255);
+    }
+}
